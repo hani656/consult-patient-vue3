d67d9aebc634e75a3225b6c319b40647725db913
haNi <2096420073@qq.com>
2023-11-01 16:44:20 +0800

    feat: 登录页面准备

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,9 @@
-<script setup lang="ts">
-import { Button as VanButton } from 'vant'
-import { useUserStore } from './stores'
-import request from './utils/request'
-import type { User } from './types/user'
-
-const store = useUserStore()
-
-const getUser = () => {
-  // axios.request({
-  //   url: 'patient/myUser',
-  //   method: 'get'
-  // })
-  const options = {
-    method: 'get',
-    data: {
-      test: 1
-    }
-  }
-  request('patient/myUser', options)
-  // request('patient/myUser', 'get', { test: 1 })
-}
-
-// 测试登录
-const login = () => {
-  // axios
-  //   .request({
-  //     url: 'login/password',
-  //     method: 'post',
-  //     data: {
-  //       mobile: '13211112222',
-  //       password: 'abc12345'
-  //     }
-  //   })
-  //
-  const options1 = {
-    // method: 'POST',
-    data: {
-      mobile: '13211112222',
-      password: 'abc12345'
-    }
-  }
-  request<User>('login/password', options1)
-    .then((res) => {
-      console.log('成功', res.data.account)
-    })
-    .catch((err) => {
-      console.log('失败', err)
-    })
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    App{{ store.user }}
-    <van-button
-      type="primary"
-      @click="
-        store.setUser({
-          id: '1',
-          avatar: '1',
-          token: '1',
-          mobile: '1',
-          account: '1'
-        })
-      "
-      >登录</van-button
-    >
-    <van-button type="primary" @click="store.delUser()">退出</van-button>
-    <van-button @click="getUser">获取用户信息</van-button>
-    <van-button @click="login">接口登录</van-button>
+  <div class="app">
+    <!-- 一级路由出口 -->
+    <router-view></router-view>
   </div>
 </template>
 
