86bd015d2ac2fe1d816b84a7f3b1bbea5ed0a32a
haNi <2096420073@qq.com>
2023-11-02 11:04:32 +0800

    feat: cp-nav-bar 功能标题和右测文字和点击右侧文字的自定义事件

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,9 @@
 <script lang="ts"></script>
 <template>
   <div class="login-page">
-    <cp-nav-bar></cp-nav-bar>
+    <cp-nav-bar title="登录" right-text="注册"></cp-nav-bar>
     <van-button>按钮</van-button>
     <van-checkbox>复选框</van-checkbox>
-    request()
   </div>
 </template>
 
