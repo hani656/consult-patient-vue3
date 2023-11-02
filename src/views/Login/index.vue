812336e0f3db4d1515722eb0147e75493065f687
haNi <2096420073@qq.com>
2023-11-02 12:11:20 +0800

    feat: cp-nav-bar 组件类型

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,17 @@
-<script lang="ts"></script>
+<script setup lang="ts">
+const fn = () => console.log(fn)
+
+// typeof 作用: 从js变量中取出对应的类型
+// const obj = {
+//   name: 'jack',
+//   age: 10
+// }
+// type Obj = typeof obj
+</script>
+
 <template>
   <div class="login-page">
-    <cp-nav-bar title="登录" right-text="注册"></cp-nav-bar>
+    <cp-nav-bar @click-right="fn" title="登录" right-text="注册"></cp-nav-bar>
     <van-button>按钮</van-button>
     <van-checkbox>复选框</van-checkbox>
   </div>
