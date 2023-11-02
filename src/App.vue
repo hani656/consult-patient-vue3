36e84ee85d7c91c39d0078135c578493f8ab24ef
haNi <2096420073@qq.com>
2023-11-02 13:44:34 +0800

    feat: 登录页面布局和剔除多余代码

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="app">
-    <router-link to="/login">login</router-link>
-    <!-- 一级路由出口 -->
-    <router-view></router-view>
-  </div>
+  <!-- 一级路由出口 -->
+  <router-view></router-view>
 </template>
 
 <style scoped lang="scss"></style>
