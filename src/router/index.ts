945593a3b9a4164f2b8acd0293e29291bbd7d5a5
haNi <2096420073@qq.com>
2023-11-03 17:25:46 +0800

    feat: 布局容器--标题设置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,5 @@
+import { useUserStore } from '@/stores'
+import { storeToRefs } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 
 // 如何得到路由实例 createRouter()
@@ -11,29 +13,53 @@ const router = createRouter({
   routes: [
     {
       path: '/login',
-      component: () => import('@/views/Login/index.vue')
+      component: () => import('@/views/Login/index.vue'),
+      meta: { title: '登录' }
     },
     {
       path: '/',
       redirect: '/home',
       component: () => import('@/views/Layout/index.vue'),
       children: [
-        { path: '/home', component: () => import('@/views/Home/index.vue') },
+        {
+          path: '/home',
+          component: () => import('@/views/Home/index.vue'),
+          meta: { title: '首页' }
+        },
         {
           path: '/article',
-          component: () => import('@/views/Article/index.vue')
+          component: () => import('@/views/Article/index.vue'),
+          meta: { title: '健康百科' }
         },
         {
           path: '/notify',
-          component: () => import('@/views/Notify/index.vue')
+          component: () => import('@/views/Notify/index.vue'),
+          meta: { title: '消息通知' }
         },
         {
           path: '/user',
-          component: () => import('@/views/User/index.vue')
+          component: () => import('@/views/User/index.vue'),
+          meta: { title: '个人中心' }
         }
       ]
     }
   ]
+})
+
+// 全局的前置守卫
+router.beforeEach((to) => {
+  // 获取 token 的
+  const store = useUserStore()
+  // 白名单
+  const whiteList = ['/login']
+  // 如果你没有token 并且不在白名单里面，重定向到登录
+  if (!store.user?.token && !whiteList.includes(to.path)) return '/login'
+})
+
+// 全局的后置守卫
+router.afterEach((to) => {
+  // 页面的标题
+  document.title = `${to.meta.title || ''}-优医问诊}`
 })
 
 export default router
