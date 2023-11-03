cb30a475f4bb4a58988e5082347f1e24fdc62d1f
haNi <2096420073@qq.com>
2023-11-03 13:55:22 +0800

    feat: svg组件的封装

---
+++ src/components/CpIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  name: String
+})
+</script>
+"
+
+<template>
+  <svg aria-hidden="true" class="cp-icon">
+    <use :href="`#icon-${name}`"></use>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+.cp-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
