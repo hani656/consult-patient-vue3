745f8c9341a4bb7ea3c3f6d9d027c174ab4ab762
HaNi <2096420073@qq.com>
2024-01-23 14:17:27 +0800

    feat：首页模块-逻辑复用

--- src/views/Home/components/DoctorCard.vue
+++ src/views/Home/components/DoctorCard.vue
@@ -1,24 +1,13 @@
 <script lang="ts" setup>
-import { followOrUnfollow } from '@/services/consult'
+import { useFollow } from '@/composables/index'
 import type { Doctor } from '@/types/consult'
-import { ref } from 'vue'
-import FollowDoctor from './FollowDoctor.vue'
 
 defineProps<{
   item: Doctor
 }>()
 
 // 关注逻辑
-const loading = ref(false)
-const follow = async (item: Doctor) => {
-  loading.value = true
-  try {
-    await followOrUnfollow(item.id)
-    item.likeFlag = item.likeFlag === 1 ? 0 : 1
-  } finally {
-    loading.value = false
-  }
-}
+const { loading, follow } = useFollow()
 </script>
 <template>
   <div class="doctor-card">
@@ -26,7 +15,13 @@ const follow = async (item: Doctor) => {
     <p class="name">{{ item.name }}</p>
     <p class="van-ellipsis">{{ item.hospitalName }} {{ item.depName }}</p>
     <p>{{ item.positionalTitles }}</p>
-    <van-button round size="small" type="primary" @click="follow(item)">
+    <van-button
+      round
+      size="small"
+      type="primary"
+      @click="follow(item)"
+      :loading="loading"
+    >
       {{ item.likeFlag === 1 ? '已关注' : '+ 关注' }}
     </van-button>
   </div>
